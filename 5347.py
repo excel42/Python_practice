cfc614c30d6231184f3f3a7da782cca910cc3de1
Gyeongjun Jang <70473658+excel42@users.noreply.github.com>
2022-10-25 13:36:06 +0900

    최소공배수 프로그램

---
+++ 5347.py
@@ -0,0 +1,12 @@
+#백준 5347 최소공배수
+def LCM(a,b):
+    return (a*b) // GCD(a,b)
+
+def GCD(a, b):
+    if b % a: return GCD(b % a,a)
+    else: return a
+    
+n = int(input())
+for i in range(n):
+    a, b = map(int, input().split())
+    print(LCM(a,b))
